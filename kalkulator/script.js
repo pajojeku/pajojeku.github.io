e18b32f93033c43c7d05071b01fe2ec8009a7446
pajojeku <pajojeq@gmail.com>
2022-10-19 09:22:25 +0200

    asdasdasd

--- kalkulator/script.js
+++ kalkulator/script.js
@@ -3,7 +3,7 @@
 function klik(nr) {
     var wyswietlacz = document.getElementById("wyswietlacz");
     var nowe = document.getElementById(nr).value;
-    if(wyswietlacz.innerHTML.length<12) {
+    if(wyswietlacz.innerHTML.length<14) {
         wyswietlacz.style.color="whitesmoke";
         wyswietlacz.innerHTML+=nowe;
     }
